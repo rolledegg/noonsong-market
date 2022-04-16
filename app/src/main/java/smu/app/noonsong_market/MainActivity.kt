a67813147ca5cfd4ca611cc9b9cad0fa6d1fb178
username <wkadlf2310@gmail.com>
2022-04-16 10:53:37 +0900

    Merge feature google login branch to develop branch
    grable 변경이랑 google.json 파일만 추가함. 기능구현은 하나도 안함

--- app/src/main/java/smu/app/noonsong_market/MainActivity.kt
+++ app/src/main/java/smu/app/noonsong_market/MainActivity.kt
@@ -4,34 +4,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    // 멤버 변수 선언
     private val  bottomNavigationView: BottomNavigationView by lazy {
         findViewById<BottomNavigationView>(R.id.bottom_navigation)
+    }
+    private val homeFragment: HomeFragment by lazy {
+       HomeFragment.newInstance()
+    }
+    private val myPageFragment: MyPageFragment by lazy {
+        MyPageFragment.newInstance()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_main)
+
+        // default fragment
+
+        supportFragmentManager.beginTransaction().add(R.id.container,homeFragment).commit()
+
 
         bottomNavigationView.setOnNavigationItemSelectedListener { item->
             when(item.itemId){
                 R.id.page_home->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container,homeFragment).commit()
+
                     true
                 }
                 R.id.page_post->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container,myPageFragment).commit()
+
                     true
                 }
                 R.id.page_chatting->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container,homeFragment).commit()
+
                     true
                 }
                 R.id.page_mypage->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container,myPageFragment).commit()
                     true
                 }
                 else -> false
--- app/src/main/java/smu/app/noonsong_market/MyPageFragment.kt
+++ app/src/main/java/smu/app/noonsong_market/MyPageFragment.kt
@@ -1,59 +1,42 @@
 package smu.app.noonsong_market
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    companion object {
+        const val TAG: String = "LOG"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        fun newInstance() : MyPageFragment {
+            return MyPageFragment()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    // 메모리에 올라갔을 떄
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG,"MyPageFragment - onCreate() called")
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG,"MyPageFragment - onAttach() called")
+    }
+
+    // 뷰가 생성되었을 떄
+    // 프레그먼트와 레이아웃을 연결시켜주는 부분
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG,"MyPageFragment - onCreateView() called")
+        val view: View = inflater.inflate(R.layout.fragment_my_page,container,false)
+        return view
     }
 }
